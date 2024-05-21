1b6e3f3727094381bee86dab5e464746a9d569b9
jy <juyoun919@gmail.com>
2024-05-21 14:17:33 +0900

    initial commit

---
+++ src/main/java/org/fullstack4/hammerteen/HammerteenApplication.java
@@ -0,0 +1,13 @@
+package org.fullstack4.hammerteen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HammerteenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HammerteenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/fullstack4/hammerteen/HammerteenApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fullstack4.hammerteen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HammerteenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
