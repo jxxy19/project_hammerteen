533553d5b5623ad4308862f39560324e1bfc87da
jy <juyoun919@gmail.com>
2024-05-27 09:19:42 +0900

    board : 전체, 내가 작성한 리스트 -> 등록 / 수정 / 삭제 / 상세 기능 추가

--- src/main/java/org/fullstack4/hammerteen/service/BbsServiceIf.java
+++ src/main/java/org/fullstack4/hammerteen/service/BbsServiceIf.java
@@ -1,7 +1,16 @@
 package org.fullstack4.hammerteen.service;
 
 
+import org.fullstack4.hammerteen.dto.BbsDTO;
+import org.fullstack4.hammerteen.dto.PageRequestDTO;
+import org.fullstack4.hammerteen.dto.PageResponseDTO;
 
 public interface BbsServiceIf {
-
+    int regist(BbsDTO bbsDTO);
+    BbsDTO view(BbsDTO bbsDTO);
+    PageResponseDTO<BbsDTO> list(PageRequestDTO pageRequestDTO);
+    void modify(BbsDTO bbsDTO);
+    void delete(BbsDTO bbsDTO);
+    PageResponseDTO<BbsDTO> myList(PageRequestDTO pageRequestDTO, String userId);
+    void updateReadCnt (int bbsIdx);
 }
